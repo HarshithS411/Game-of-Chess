820a685892af4efc1cd481ebe1a5b737118c96c0
Blessto296 <blesstofearethd@gmail.com>
2024-03-12 13:41:35 +0530

    openning match

---
+++ game.py
@@ -0,0 +1,29 @@
+import  pygame
+
+
+
+
+pygame.init()
+WIDTH = 1000
+HEIGHT = 900
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('two-player pygame chess!')
+font = pygame.font.Font('freesansbold.ttf',20)
+big_font = pygame.font.Font('freesansbold.ttf',50)
+timer = pygame.time.clock()
+fps = 60
+#game variables and images
+
+#main game loop
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('dark gray')
+
+    #event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()    
