eae589281d366f1220ae9353af61a12a4854db7f
BalaVinay18 <vinayvinu0046@gmail.com>
2024-03-25 12:05:42 +0530

    pen

--- main.py
+++ main.py
@@ -13,11 +13,17 @@ fps = 60
 # Game varaibles and images
 white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
                 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
-# whoever is starting, start in this line and in video 10:20
+white_location = [(0,0),(1, 0), (2, 0), (3, 0), (4, 0), (5,0), (6, 0), (7, 0),
+                  (0,1),(1, 1), (2, 1), (3, 1), (4, 1), (5,1), (6, 1), (7, 1)]
 black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
                 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
-
-
+black_location = [(0,7),(1, 7), (2, 7), (3, 7), (4, 7), (5,7), (6, 7), (7, 7),
+                  (0,6),(1, 6), (2, 6), (3, 6), (4, 6), (5,6), (6,6), (7, 6)]
+captured_pieces_white = []
+captured_pieces_white = []
+turn_step = 0 
+selection = 100
+valid_moves = []
 # Main game loop
 run = True
 while run:
