61371c50cdbfae1d8dbbc291420dc58a23e47430
HarshithS411 <159102941+HarshithS411@users.noreply.github.com>
2024-03-28 05:59:24 +0530

    6

--- main.py
+++ main.py
@@ -21,42 +21,42 @@ black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight',
 black_location = [(0,7),(1, 7), (2, 7), (3, 7), (4, 7), (5,7), (6, 7), (7, 7),
                   (0,6),(1, 6), (2, 6), (3, 6), (4, 6), (5,6), (6,6), (7, 6)]
 captured_pieces_white = []
-captured_pieces_white = []
+captured_pieces_black = []
 turn_step = 0 
 selection = 100
 valid_moves = []
 black_queen = pygame.image.load('assets/images/black queen.png')
-black_queen = pygame.transform.scale(black_queen,(80,80))
+black_queen = pygame.transform.scale(black_queen,(65,65))
 black_queen_small = pygame.transform.scale(black_queen,(45,45))
 black_king = pygame.image.load('assets/images/black king.png')
-black_king = pygame.transform.scale(black_queen,(80,80))
+black_king = pygame.transform.scale(black_queen,(65,65))
 black_king_small = pygame.transform.scale(black_queen,(45,45))
 black_rook = pygame.image.load('assets/images/black rook.png')
-black_rook = pygame.transform.scale(black_rook,(80,80))
+black_rook = pygame.transform.scale(black_rook,(65,65))
 black_rook_small = pygame.transform.scale(black_rook,(45,45))
 black_bishop = pygame.image.load('assets/images/black bishop.png')
-black_bishop = pygame.transform.scale(black_bishop,(80,80))
+black_bishop = pygame.transform.scale(black_bishop,(65,65))
 black_bishop_small = pygame.transform.scale(black_bishop,(45,45))
 black_knight = pygame.image.load('assets/images/black knight.png')
-black_knight = pygame.transform.scale(black_knight,(80,80))
+black_knight = pygame.transform.scale(black_knight,(65,65))
 black_knight_small = pygame.transform.scale(black_knight,(45,45))
 black_pawn = pygame.image.load('assets/images/black pawn.png')
 black_pawn = pygame.transform.scale(black_pawn,(65,65))
 black_pawn_small = pygame.transform.scale(black_pawn,(45,45))
 white_queen = pygame.image.load('assets/images/white queen.png')
-white_queen = pygame.transform.scale(white_queen,(80,80))
+white_queen = pygame.transform.scale(white_queen,(65,65))
 white_queen_small = pygame.transform.scale(white_queen,(45,45))
 white_king = pygame.image.load('assets/images/white king.png')
-white_king = pygame.transform.scale(white_king,(80,80))
+white_king = pygame.transform.scale(white_king,(65,65))
 white_king_small = pygame.transform.scale(white_king,(45,45))
 white_rook = pygame.image.load('assets/images/white rook.png')
-white_rook = pygame.transform.scale(white_rook,(80,80))
+white_rook = pygame.transform.scale(white_rook,(65,65))
 white_rook_small = pygame.transform.scale(white_rook,(45,45))
 white_bishop = pygame.image.load('assets/images/white bishop.png')
-white_bishop = pygame.transform.scale(white_bishop,(80,80))
+white_bishop = pygame.transform.scale(white_bishop,(65,65))
 white_bishop_small = pygame.transform.scale(white_bishop,(45,45))
 white_knight = pygame.image.load('assets/images/white knight.png')
-white_knight = pygame.transform.scale(white_knight,(80,80))
+white_knight = pygame.transform.scale(white_knight,(65,65))
 white_knight_small = pygame.transform.scale(white_knight,(45,45))
 white_pawn = pygame.image.load('assets/images/white pawn.png')
 white_pawn = pygame.transform.scale(white_pawn,(65,65))
@@ -93,21 +93,49 @@ def draw_board():
 # draw pieces onto board
 def draw_pieces():
     for i in range(len(white_pieces)):
-        index = piece_list.index(white_pieces[i])
         if white_pieces[i] == 'pawn':
-            screen.blit(white_pawn, (white_location[i][0] * 100 + 22, white_location[i][1] * 100 + 30))
-        else:
-            screen.blit(white_images[index], (white_location[i][0] * 100 + 10, white_location[i][1] * 100 + 10))
+            screen.blit(white_pawn, (white_location[i][0] * 100 + 22, white_location[i][1] * 80 + 5))
+        elif white_pieces[i] == 'rook':
+            screen.blit(white_rook, (white_location[i][0] * 100, white_location[i][1] * 80))
+        elif white_pieces[i] == 'knight':
+            screen.blit(white_knight, (white_location[i][0]*100 , white_location [i ][1]*80)) 
+        elif white_pieces[i] == 'bishop':
+             screen.blit(white_bishop, (white_location[i][0]*100 , white_location[i][1]*80)) 
+        elif   white_pieces[i]== 'queen':
+            screen.blit(white_queen, (white_location[i][0] * 100, white_location[i][1] * 80))
+        elif   white_pieces[i] == 'king':
+             screen.blit(white_king, (white_location[i][0]*100 , white_location[i][1]*80))
+        if turn_step >= 2:
+            if selection == i:
+                pygame.draw.rect(screen, 'red', [black_location[i][0] * 100 + 1, black_location[i][1] * 100 + 1, 50, 50], 2)
+
+
 
     for i in range(len(black_pieces)):
-        index = piece_list.index(black_pieces[i])
         if black_pieces[i] == 'pawn':
-            screen.blit(black_pawn, (black_location[i][0] * 100 + 22, black_location[i][1] * 100 + 30))
-        else:
-            screen.blit(black_images[index], (black_location[i][0] * 100 + 10, black_location[i][1] * 100 + 10))
+            screen.blit(black_pawn, (black_location[i][0] * 100 + 22, black_location[i][1] * 80 + 5))
+        elif black_pieces[i] == 'rook':
+            screen.blit(black_rook, (black_location[i][0]*100 , black_location[i][1]*80 + 5)) 
+        elif black_pieces[i] == 'knight':
+             screen.blit(black_knight, (black_location[i][0]*100 , black_location[i][1]*80 + 5 )) 
+        elif   black_pieces[i] == 'bishop':
+             screen.blit(black_bishop, (black_location[i][0]*100 , black_location[i][1]*80 + 5 )) 
+        elif   black_pieces[i] == 'queen':
+             screen.blit(black_queen ,(black_location[i][0]*100 , black_location[i][1]*80 + 5 )) 
+        elif   black_pieces[i] == 'king':
+             screen.blit(black_king, (black_location[i][0]*100 , black_location[i][1]*80 + 5 ))
+        if turn_step < 2:
+            if selection == i:
+                pygame.draw.rect(screen, 'red', [white_location[i][0] * 100 + 1, white_location[i][1] * 100 + 1, 100, 100], 2)
+
+# function to check all pieces valid options on board
+def check_options():
+    pass
 
 
 # Main game loop
+black_options = check_options(black_pieces, black_location, 'black')
+white_options = check_options(white_pieces, white_location, 'white')
 run = True
 while run:
     timer.tick(fps)
@@ -119,6 +147,45 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x_coord = event.pos[0] // 100
+            y_coord = event.pos[1] // 100
+            click_coord = (x_coord, y_coord)
+            if turn_step <= 1:
+                if click_coord in white_location:
+                    selection = white_location.index(click_coord)
+                    if turn_step == 0:
+                        turn_step = 1
+                if click_coord in valid_moves and selection != 100:
+                    white_location[selection] = click_coord
+                    if click_coord in black_location:
+                        black_pieces = black_location.index(click_coord)
+                        captured_pieces_white.append(black_pieces[black_pieces])
+                        black_pieces.pop(black_pieces)
+                        black_location.pop(black_pieces)
+                    black_options = check_options(black_pieces, black_location, 'black')
+                    white_options = check_options(white_pieces, white_location, 'white')
+                    turn_step = 2
+                    selection = 100
+                    valid_moves = []
+            if turn_step > 1:
+                if click_coord in black_location:
+                    selection = black_location.index(click_coord)
+                    if turn_step == 2:
+                        turn_step = 3
+                if click_coord in valid_moves and selection != 100:
+                    black_location[selection] = click_coord
+                    if click_coord in white_location:
+                        white_pieces = white_location.index(click_coord)
+                        captured_pieces_black.append(white_pieces[white_pieces])
+                        white_pieces.pop(white_pieces)
+                        white_location.pop(white_pieces)
+                    black_options = check_options(black_pieces, black_location, 'black')
+                    white_options = check_options(white_pieces, white_location, 'white')
+                    turn_step = 2
+                    selection = 100
+                    valid_moves = []
+
             
     pygame.display.flip()
 pygame.quit()
